d9f60d5d96790e7a11e0f6ea300c69ba761b368c
{GaHyoung} <[mnb2457@naver.com](mailto:mnb2457@naver.com)>
2024-02-19 16:15:33 +0900

    banner 미완

--- src/main/java/com/nhnacademy/edu/springboot/student/StudentApplication.java
+++ src/main/java/com/nhnacademy/edu/springboot/student/StudentApplication.java
@@ -6,16 +6,13 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication //spring-boot 실행의 시작점
-public class StudentApplication extends SpringBootServletInitializer {
+public class StudentApplication  {
 
 	public static void main(String[] args) {
 		//spring-boot 시작 명령.
 		//args를 넣지않으면 외부에서 넣는 인자가 넘어오지 않는 문제 발생할 수 있음.
-		SpringApplication.run(StudentApplication.class, args);
+		SpringApplication application = new SpringApplication(StudentApplication.class);
+		application.run(args);
 	}
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(StudentApplication.class);
-	}
 }
